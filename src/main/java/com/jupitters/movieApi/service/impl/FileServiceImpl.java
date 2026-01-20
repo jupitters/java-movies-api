77ed8bd6c43345356c37ad62943d646ad115550a
jupitters <sampaiobale@gmail.com>
2026-01-20 11:41:12 -0300

    file service impl

--- src/main/java/com/jupitters/movieApi/service/impl/FileServiceImpl.java
+++ src/main/java/com/jupitters/movieApi/service/impl/FileServiceImpl.java
@@ -1,12 +1,14 @@
 package com.jupitters.movieApi.service.impl;
 
 import com.jupitters.movieApi.service.FileService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Service
 public class FileServiceImpl implements FileService {
     @Override
     public String uploadFile(String path, MultipartFile file) throws IOException {
