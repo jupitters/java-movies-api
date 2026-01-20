2465f41e5af3f698bcc9d1f6c8784b715be0ea6a
jupitters <sampaiobale@gmail.com>
2026-01-20 17:40:45 -0300

    movie controller

---
+++ src/main/java/com/jupitters/movieApi/controller/MovieController.java
@@ -0,0 +1,30 @@
+package com.jupitters.movieApi.controller;
+
+import com.jupitters.movieApi.dto.MovieDto;
+import com.jupitters.movieApi.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.ObjectMapper;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/movie")
+public class MovieController {
+    private final MovieService movieService;
+
+    @PostMapping("/add")
+    public ResponseEntity<MovieDto> addMovieHandler(@RequestPart MultipartFile file, @RequestPart String movieDto){
+
+    }
+
+    private MovieDto convertToMovieDto(String movieDtoObj){
+        ObjectMapper objectMapper = new ObjectMapper();
+        MovieDto movieDto = objectMapper.readValue(movieDtoObj, MovieDto.class);
+        return movieDto;
+    }
+}
