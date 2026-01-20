2ddd25c0d3ed0fd75e321d673a2d17461d20df8c
jupitters <sampaiobale@gmail.com>
2026-01-20 17:44:21 -0300

    movie controller

--- src/main/java/com/jupitters/movieApi/controller/MovieController.java
+++ src/main/java/com/jupitters/movieApi/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.jupitters.movieApi.controller;
 import com.jupitters.movieApi.dto.MovieDto;
 import com.jupitters.movieApi.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import tools.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/movie")
@@ -18,8 +21,10 @@ public class MovieController {
     private final MovieService movieService;
 
     @PostMapping("/add")
-    public ResponseEntity<MovieDto> addMovieHandler(@RequestPart MultipartFile file, @RequestPart String movieDto){
-
+    public ResponseEntity<MovieDto> addMovieHandler(@RequestPart MultipartFile file, @RequestPart String movieDto) throws IOException {
+        MovieDto dto = convertToMovieDto(movieDto);
+        MovieDto response = movieService.addMovie(dto, file);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     private MovieDto convertToMovieDto(String movieDtoObj){
