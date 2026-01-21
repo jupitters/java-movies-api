9d4195552a151650e4d67c47266778b93c62d500
jupitters <sampaiobale@gmail.com>
2026-01-21 11:39:57 -0300

    refresh token

--- src/main/java/com/jupitters/movieApi/auth/model/RefreshToken.java
+++ src/main/java/com/jupitters/movieApi/auth/model/RefreshToken.java
@@ -1,7 +1,23 @@
 package com.jupitters.movieApi.auth.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.Instant;
 
 @Entity
 public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    @NotBlank(message = "refresh token needed")
+    private String refreshToken;
+    @Column(nullable = false)
+    private Instant expirationTime;
+
+    @OneToOne
+    private User user;
+
 }
