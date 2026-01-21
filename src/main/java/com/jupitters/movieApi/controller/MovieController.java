f63a450604f2e0a43258db6f274cdee0a4ee4d57
jupitters <sampaiobale@gmail.com>
2026-01-21 09:21:12 -0300

    movie service

--- src/main/java/com/jupitters/movieApi/controller/MovieController.java
+++ src/main/java/com/jupitters/movieApi/controller/MovieController.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,9 +26,12 @@ public class MovieController {
     }
 
     @GetMapping("/{movieId}")
-    public ResponseEntity<MovieDto> getMovie(Long movieId){
+    public ResponseEntity<MovieDto> getMovie(@PathVariable Long movieId){
         return ResponseEntity.ok(movieService.getMovie(movieId));
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<MovieDto>> getAllMovies(){}
 
     private MovieDto convertToMovieDto(String movieDtoObj){
         ObjectMapper objectMapper = new ObjectMapper();
