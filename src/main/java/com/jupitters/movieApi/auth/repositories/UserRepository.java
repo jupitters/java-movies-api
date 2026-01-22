c2e544eb24ebfbdd4c89566d5121771058016b02
jupitters <sampaiobale@gmail.com>
2026-01-22 09:35:49 -0300

    user repository

--- src/main/java/com/jupitters/movieApi/auth/repositories/UserRepository.java
+++ src/main/java/com/jupitters/movieApi/auth/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.jupitters.movieApi.auth.repositories;
 import com.jupitters.movieApi.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
