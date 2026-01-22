dca1236acbfff2eb998803996d619482d83c0368
jupitters <sampaiobale@gmail.com>
2026-01-22 10:54:26 -0300

    user model

--- src/main/java/com/jupitters/movieApi/auth/model/User.java
+++ src/main/java/com/jupitters/movieApi/auth/model/User.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -36,7 +37,7 @@ public class User implements UserDetails {
 
     @OneToOne(mappedBy = "user")
     private RefreshToken refreshToken;
-
+    
     private boolean isEnabled = true;
     private boolean isAccountNonExpired = true;
     private boolean isAccountNonLocked = true;
@@ -47,7 +48,8 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
