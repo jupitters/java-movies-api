0f62295251bd6618a44623d3e97f4a9e6995699d
jupitters <sampaiobale@gmail.com>
2026-01-22 12:34:59 -0300

    forgot passowrd service

--- src/main/java/com/jupitters/movieApi/service/impl/EmailService.java
+++ src/main/java/com/jupitters/movieApi/service/impl/EmailService.java
@@ -2,6 +2,7 @@ package com.jupitters.movieApi.service.impl;
 
 import com.jupitters.movieApi.dto.MailBody;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,13 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     private final JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+
     public void sendSimpleMessage(MailBody mailBody){
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(mailBody.to());
-        message.setFrom("");
+        message.setFrom(emailFrom);
         message.setSubject(mailBody.subject());
         message.setText(mailBody.text());
 
