49193c7a726e6740b87a65dfffb3d5133058f7b5
jupitters <sampaiobale@gmail.com>
2026-01-22 10:04:19 -0300

    jwt serice

--- src/main/java/com/jupitters/movieApi/auth/service/JwtService.java
+++ src/main/java/com/jupitters/movieApi/auth/service/JwtService.java
@@ -1,7 +1,16 @@
 package com.jupitters.movieApi.auth.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    @Value("${auth.token.jwtSecret}")
+    private String SECRET_KEY;
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
 }
